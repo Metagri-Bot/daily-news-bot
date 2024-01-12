d370fe4965240daa94b39b17f634f25e2c60c657
Glitch (glitch-hello-node) <none>
2024-01-12 08:13:35 +0000

    🙈🍞 Checkpoint ./main.js:54595121/4047 ./server.js:54595121/5306

--- main.js
+++ main.js
@@ -1,66 +1,85 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const http = require("http");
 const axios = require("axios");
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const GAS_API_URL = process.env.GAS_API_URL;
-const MESSAGE_SEND_CHANNEL_ID = process.env.MESSAGE_SEND_CHANNEL_ID;
 const client = new Client({
   intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b)
 });
 
-const TARGET_ROLE_ID = process.env.ROLE_ID;
-
-
+const TARGET_ROLE_ID = "1195192585594081300";
+const TARGET_CHANNEL_IDS = [1195186452691177582,1195186471708131409,1195186490607685743,1195186507321966633,1195186524258578513,1195186542545748088,1195186544064077944,1195186560547692605]
 
 client.once("ready", () => {
   console.log('Bot is ready!');
 });
 client.login(DISCORD_BOT_TOKEN);
 
-
-
 client.on("messageCreate", async message => {
   if(message.author.bot) return;
-  if (! message.member.roles.cache.has(TARGET_ROLE_ID)) return;
-  
+  if (!message.member.roles.cache.has(TARGET_ROLE_ID)) return;
   let workNum = 0;
   const userId = message.author.id;
   const userName = message.author.tag;
   const content = message.content;
   const channelId = message.channel.id;
   const file = message.attachments.first();
-  let inImageFile = false; 
+  let isInImageFile = false; 
   
   // 添付ファイルの有無を調べる
   if(file){
       // 添付ファイルが画像ファイルか調べる
     if (file.height && file.width){
-      inImageFile = true;
+      isInImageFile = true;
     }
   }
   
-  if(channelId == 0){
+  const isReplay = message.reference;
+  
+  if(channelId == TARGET_CHANNEL_IDS[0]){
     workNum = 0;
   }
-  else if(channelId == 0){
-    workNum = 2;
+  else if(channelId == TARGET_CHANNEL_IDS[1] && isReplay){
+    workNum = 1;
   }
-  else if(channelId == 0){
-    workNum = 4;
+  else if(channelId == TARGET_CHANNEL_IDS[2]){
+    if(isInImageFile){
+      workNum = 2;
+    }
+    else if(isReplay){
+      workNum = 3;
+    }
+    else{
+      return;
+    }
   }
-  else if(channelId == 0){
-    workNum = 6;
+  else if(channelId == TARGET_CHANNEL_IDS[3]){
+    if(isReplay){
+      workNum = 5;
+    }
+    else{
+      workNum = 4;
+    }
   }
-  else if(channelId == 0){
+  else if(channelId == TARGET_CHANNEL_IDS[4]){
+    if(isReplay){
+      workNum = 7;
+    }
+    else{
+      workNum = 6;
+    }
+  }
+  else if(channelId == TARGET_CHANNEL_IDS[5]){
     workNum = 8;
   }
-  else if(channelId == 0){
+  else if(channelId == TARGET_CHANNEL_IDS[6]){
     workNum = 9;
   }
-  else if(channelId == 0){
+  else if(channelId == TARGET_CHANNEL_IDS[7]){
     workNum = 10;
   }
-  
+  else{
+    return;
+  }
   let isAuto = true;
   const data={
     workNum: workNum,
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const http = require("http");
 const { Client, GatewayIntentBits } = require("discord.js");
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const client = new Client({
   intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b)
 });
 
-const CHANNEL_ID = 1181411208419606640;
-const CHANNEL = client.channels.cache.get(CHANNEL_ID);
+const CHANNEL_ID = "1181411208419606640";
+const PER_30_URL = "https://discord.com/channels/1107480974343815299/1195272027460874353";
+const PER_60_URL = "https://discord.com/channels/1107480974343815299/1195272027460874353";
+const PER_100_URL = "https://discord.com/channels/1107480974343815299/1195272027460874353";
+
+client.once("ready", () => {
+  console.log('Bot is ready!');
+});
+client.login(DISCORD_BOT_TOKEN);
 
 http
   .createServer((request, response) => {
@@ -22,6 +30,7 @@ http
 
       // リクエストデータの受信が完了した際に発生する 'end' イベントのハンドラを設定
       request.on("end", () => {
+        const channel = client.channels.cache.get(CHANNEL_ID);
         const data = JSON.parse(requestBody);
         const userData = data.postData;
         
@@ -30,15 +39,22 @@ http
           let works = userData[i].works;
           let numPoint = userData[i].numPoint;
           let totalPoint = userData[i].totalPoint;
-          let addMessage = userData[i].addMessage;
-          
-          let message =`${userId}さん\n本日のMetaGreenSeedsポイントを配布します。\n\n内訳：\n`;
+          let overPoint = userData[i].overPoint;
+          let message =`<@${userId}> さん\n本日のMetaGreenSeedsポイントを配布します。\n\n内訳：\n`;
           for(let j = 0; j < works.length; j++){
             message = message + `${works[j]}\n`
           } 
           message = message + `\n現在の合計MetaGreenSeedsポイントは ${totalPoint} ポイントです。\n`;
-          message = message + `\n${addMessage}`;
-          CHANNEL.send(message);
+          if(overPoint == 30){
+            message = message + `\n【お知らせ】MetaGreenSeedsポイントが30ポイント溜ってます。\nこちら ${PER_30_URL} をご確認ください。`;
+          }
+          if(overPoint == 60){
+            message = message + `\n【お知らせ】MetaGreenSeedsポイントが60ポイント溜ってます。\nこちら ${PER_60_URL} をご確認ください。`;
+          }
+          if(overPoint == 100){
+            message = message + `\n【お知らせ】MetaGreenSeedsポイントが100ポイント溜ってます。\nこちら ${PER_100_URL} をご確認ください。`;
+          }
+          channel.send(message);
         }
       });
 
